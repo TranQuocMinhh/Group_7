7c93f890ffdba6cd9c6c7cd2b8a375095df8c609
minnnguyen <161324800+minnnguyen@users.noreply.github.com>
2024-03-10 22:32:09 +0700

    Add files via upload

---
+++ CMU-CS 246 FIS - Team 7-20240226T125917Z-001/CMU-CS 246 FIS - Team 7/Process/3.Code/update_pow.java
@@ -0,0 +1,24 @@
+package linhtinh;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class update_pow {
+	Square.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			try {
+				double first = Double.parseDouble(t1.getText());
+//				double second = Double.parseDouble(t2.getText());
+				if(e.getSource() == Square || e.getSource() == t1) {
+					double result = Math.pow(first, 2);
+					t1.setText(""+result);
+//					double result2 = Math.pow(second, 2);
+//					t2.setText(""+result2);
+				}
+				
+			} catch (NumberFormatException ex) {
+				t3.setText("Error");
+			}
+		}
+	});
+}
